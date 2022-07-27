3900d2dc448245511d90aabe28f45d9dc7a2aee9
HartMason <hartmason85@gmail.com>
2022-07-26 21:51:10 -0500

    practicing using fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,55 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import { render } from "@testing-library/react";
 
 function App() {
-  return <div className="App"></div>;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api?results=25")
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      console.log("server says bad response");
+    })
+    .then((res) => setData(res.data))
+    .catch((err) => console.log("didn't work"));
+  },[])
+
+
+
+
+  // const [users, fetchUsers] = useState([])
+
+  // const fetchData = () => {
+  //   fetch('https://randomuser.me/api?results=25')
+  //   .then(response => response.json())
+  //   .then(parsedJSON =>parsedJSON.results.map( user => (
+  //     {
+  //     name: `${user.name.first} ${user.name.last}`,
+  //     location: `${user.street.number} ${user.street.name}`
+  //   }
+  //   )))
+  //   .then(contacts => this.setState({
+  //     contacts,
+  //     isLoading: false
+  //   }))
+  //   console.log(fetchData)
+
+  // const {user, fetchUsers} = this.state
+  render()
+  return (
+    <div className="App">
+      <header>Address Book</header>
+      <div>
+        {data && data.map(() => (userData) => {
+          return <div>{userData}</div>;
+        })}
+      </div>
+      <ol></ol>
+    </div>
+  );
 }
 
 export default App;
