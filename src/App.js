6049baed68d1e7d63f9cea5b3d50e6eda2931b22
HartMason <hartmason85@gmail.com>
2022-07-26 21:57:53 -0500

    cleaned up fetch request

--- src/App.js
+++ src/App.js
@@ -3,22 +3,13 @@ import React, { useState, useEffect } from "react";
 import { render } from "@testing-library/react";
 
 function App() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch("https://randomuser.me/api?results=25")
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      console.log("server says bad response");
-    })
-    .then((res) => setData(res.data))
-    .catch((err) => console.log("didn't work"));
-  },[])
-
-
-
+    fetch("https://randomuser.me/api?results=25").then((res) => {
+      setData(res.data.results);
+    });
+  }, []);
 
   // const [users, fetchUsers] = useState([])
 
@@ -38,14 +29,15 @@ function App() {
   //   console.log(fetchData)
 
   // const {user, fetchUsers} = this.state
-  render()
+  render();
   return (
     <div className="App">
       <header>Address Book</header>
       <div>
-        {data && data.map(() => (userData) => {
-          return <div>{userData}</div>;
-        })}
+        {data &&
+          data.map(() => (userData) => {
+            return <div>{userData}</div>;
+          })}
       </div>
       <ol></ol>
     </div>
